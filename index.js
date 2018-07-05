c3d39ff9831f622ca1fbe3c214fb8a8cb51ceacd
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-05 01:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function shout(string) {
-  console.log(string.toUpperCase)
-}
+function logShout(string) {
+  console.log() string.toUpperCase
